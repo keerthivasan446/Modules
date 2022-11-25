6e443dc35f77527147501e4463c29432a00985a9
keer9329 <keerthivasan.v@rackspace.com>
2022-11-25 12:25:14 +0530

    format

--- web/main.tf
+++ web/main.tf
@@ -16,7 +16,7 @@ module "network" {
   secondary_subnet_cidr = var.secondary_subnet_cidr
   tertiary_subnet_name  = var.tertiary_subnet_name
   tertiary_subnet_cidr  = var.tertiary_subnet_cidr
-  primary_extra_tags    = var. primary_extra_tags
+  primary_extra_tags    = var.primary_extra_tags
   secondary_extra_tags  = var.secondary_extra_tags
   tertiary_extra_tags   = var.tertiary_extra_tags
 }
@@ -72,33 +72,33 @@ module "web-app" {
 
 module "org-db" {
 
-  source                                                    = "../modules/rds"
-  org_name                                                  = var.org_name
-  env                                                       = var.env
-  name                                                      = var.name
-  instance_class                                            = var.instance_class
-  iops                                                      = var.iops
-  allocated_storage                                         = var.allocated_storage
-  storage_type                                              = var.storage_type
-  storage_encrypted                                         = var.storage_encrypted
-  allow_major_version_upgrade                               = var.allow_major_version_upgrade
-  auto_minor_version_upgrade                                = var.auto_minor_version_upgrade
-  apply_immediately                                         = var.apply_immediately
-  backup_retention_period                                   = var.backup_retention_period
-  backup_window                                             = var.backup_window
-  maintenance_window                                        = var. maintenance_window
-  copy_tags_to_snapshot                                     = var.copy_tags_to_snapshot
-  cloudwatch_logs_exports                                   = var.cloudwatch_logs_exports
-  engine                                                    = var.engine
-  engine_version                                            = var.engine_version
-  iam_authentication_enabled                                = var.iam_authentication_enabled
-  username                                                  = var.username
-  admin_password                                            = random_password.password.result
-  multi_az                                                  = var.multi_az
-  skip_final_snapshot                                       = var.skip_final_snapshot
-  subnet_ids                                                = module.network.tertiary_subnet_ids
-  vpc_id                                                    = module.network.vpc_id
-  ingress_security_groups                                   = [module.web-app.lb_security_group]
-  db_port                                                   = var.db_port
-  db_ingress_asg_protocol                                   = var.db_ingress_asg_protocol
+  source                      = "../modules/rds"
+  org_name                    = var.org_name
+  env                         = var.env
+  name                        = var.name
+  instance_class              = var.instance_class
+  iops                        = var.iops
+  allocated_storage           = var.allocated_storage
+  storage_type                = var.storage_type
+  storage_encrypted           = var.storage_encrypted
+  allow_major_version_upgrade = var.allow_major_version_upgrade
+  auto_minor_version_upgrade  = var.auto_minor_version_upgrade
+  apply_immediately           = var.apply_immediately
+  backup_retention_period     = var.backup_retention_period
+  backup_window               = var.backup_window
+  maintenance_window          = var.maintenance_window
+  copy_tags_to_snapshot       = var.copy_tags_to_snapshot
+  cloudwatch_logs_exports     = var.cloudwatch_logs_exports
+  engine                      = var.engine
+  engine_version              = var.engine_version
+  iam_authentication_enabled  = var.iam_authentication_enabled
+  username                    = var.username
+  admin_password              = random_password.password.result
+  multi_az                    = var.multi_az
+  skip_final_snapshot         = var.skip_final_snapshot
+  subnet_ids                  = module.network.tertiary_subnet_ids
+  vpc_id                      = module.network.vpc_id
+  ingress_security_groups     = [module.web-app.lb_security_group]
+  db_port                     = var.db_port
+  db_ingress_asg_protocol     = var.db_ingress_asg_protocol
 }
